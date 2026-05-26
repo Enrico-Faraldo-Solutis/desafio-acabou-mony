a89862ef09a53b15578493019e2956d92494d2e8
fe-hideki <felipe.dsouza@sptech.school>
2026-05-26 10:45:26 -0300

    first commit

---
+++ src/main/java/com/example/acabou_o_mony_api/AcabouOMonyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.acabou_o_mony_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcabouOMonyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcabouOMonyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/acabou_o_mony_api/AcabouOMonyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acabou_o_mony_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcabouOMonyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
